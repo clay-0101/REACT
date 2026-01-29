5d3d4830d2969ad4c58056a1da2f89edbb006b3d
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-01-29 20:04:53 +0530

    Add Advance useStat concept

--- 08-useState/src/App.jsx
+++ 08-useState/src/App.jsx
@@ -1,22 +1,52 @@
 import { useState } from "react"
 
 const App = () => {
+
+// THIS IS THE FIRST EXAMPLE
   const [num, setnum] = useState(0)
-  const incrementNum = ()=>{
-    setnum(num+1)
+  const incrementNum = () => {
+    setnum(num + 1)
   }
-  const decrementNum = ()=>{
-    setnum(num-1)
+  const decrementNum = () => {
+    setnum(num - 1)
   }
-  const power2Num= ()=>{
-    setnum(num*num)
+  const power2Num = () => {
+    setnum(num * num)
   }
+
+// THIS IS THE SECOND EXAMPLE
+  const [title, settitle] = useState('')
+  const submiting = (e)=>{
+    e.preventDefault()
+    console.log("form submitted...")
+  }
+
+
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={incrementNum }>INCREMNET</button>
-      <button onClick={decrementNum}>DECREMNET</button>
-      <button onClick={power2Num}>power-2</button>
+      {/* FOR FIRST FUNCTION */}
+      <div>
+        <h1>{num}</h1>
+        <button onClick={incrementNum}>INCREMNET</button>
+        <button onClick={decrementNum}>DECREMNET</button>
+        <button onClick={power2Num}>power-2</button>
+      </div>
+      {/* FOR SECOND FUNCTION */}
+      <div>
+        <form onSubmit={(e)=>{
+          submiting(e)
+        }}>
+          <input 
+          type="text" 
+          placeholder="Enter your name"
+          value = {title}
+          onChange={(e)=>{
+            console.log(e.target.value)
+          }}
+          />
+          <button>submit</button>
+        </form>
+      </div>
     </div>
   )
 }
